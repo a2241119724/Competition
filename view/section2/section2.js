a74e835cdfe206782b39a6906c37bc3d93d4997f
liu-anbo <2241119724@qq.com>
2020-11-08 19:27:24 +0800

    text+optimize

--- view/section2/section2.js
+++ view/section2/section2.js
@@ -11,20 +11,13 @@ $(function() {
     let arr_height = []; //存储每列的高度
     let minHeight = 0; //存取每次更新后数组的最小值
     let indexHeight = 0; //存取每次更新后数组的最小值的索引
-    let paddingofhalf = (img_width - $li_last.width()) / 2; //获取每个包裹盒子padding/2的宽度
-    let imgName = 0; //图片的名字
     let count = 0; //图片的数量
     let ul_width = img_width * cols_img; //ul的宽度,使之居中
     // let clock; //函数节流
 
-    // $ul_img.css({
-    //     "width": `${ul_width}%`,
-    //     "margin": `0 ${(100-ul_width)/2}%`,
-    //     "transform": `translate(${-paddingofhalf}px)`,
-    // });
     $ul_img.css({
         "width": ul_width + "px",
-        "transform": "translate(" + (-paddingofhalf) + "px)",
+        // "transform": "translate(" + (-(img_width - $li_last.width()) / 2) + "px)",
     });
 
     //初始化图片
@@ -41,27 +34,6 @@ $(function() {
 
     function refresh() {
         arr_height = [];
-        // $ul_img.find("li").each((index, item) => { //遍历图片li
-        //     if (index < cols_img) {
-        //         arr_height.push($(item).innerHeight());
-        //     } else {
-        //         minHeight = Math.min(...arr_height);
-        //         indexHeight = arr_height.indexOf(minHeight);
-        //         // $(item).css({
-        //         //     "position": "absolute",
-        //         //     "top": `${minHeight}px`,
-        //         //     "left": `${indexHeight * img_width}px`,
-        //         // });
-        //         $(item).css({
-        //             "position": "absolute",
-        //             "top": minHeight + "px",
-        //             "left": indexHeight * img_width + "px",
-        //         });
-        //         arr_height[indexHeight] += $(item).innerHeight();
-        //         //改变包裹li的ul的高度
-        //         // $ul_img.css({ "height": `${$waterfall.scrollTop()+ $waterfall.innerHeight()}px` });
-        //     }
-        // })
         $ul_img.find("li").each(function(index, item) { //遍历图片li
             if (index < cols_img) {
                 arr_height.push($(item).innerHeight());
@@ -71,11 +43,6 @@ $(function() {
                 // minHeight = Math.min(...arr_height);
                 minHeight = Math.min.apply(null, arr_height);
                 indexHeight = arr_height.indexOf(minHeight);
-                // $(item).css({
-                //     "position": "absolute",
-                //     "top": `${minHeight}px`,
-                //     "left": `${indexHeight * img_width}px`,
-                // });
                 $(item).css({
                     "position": "absolute",
                     "top": minHeight + "px",
@@ -85,46 +52,11 @@ $(function() {
                 //改变包裹li的ul的高度
                 // $ul_img.css({ "height": `${$waterfall.scrollTop()+ $waterfall.innerHeight()}px` });
             }
-        })
-        $("#page2 .waterfall li:not(:last-child)").not("[is-loding]").each(function() {
-            // 点击在旁边的盒子放大
-            // $(this).off("click").click(function(event) {
-            //     let e = event || window.e;
-            //     let date = new Date();
-            //     let newDate = date.getFullYear() + "月" + date.getMonth() + "月" + date.getDay() + "日 " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + ":" + date.getMilliseconds();
-            //     $beBig.find("img").attr("src", $(e.currentTarget).find("img").attr("src"));
-            //     $beBig.find(".message h4").text($(e.currentTarget).find(".message h4").text());
-            //     $beBig.find(".message p").html(newDate + "<br/>" + $(e.currentTarget).find(".message p").text());
-            // });
         });
-        // setTimeout(function() {
-        //     $("#page2 .waterfall li:not(:last-child)").not("[is-loding]").each(function() {
-        //         $(this).attr("is-loding", 1); //截断(只循环刚生成的li)
-        //         // $(this).find(".wrap img").load(function() {
-        //         if ($("#li" + $(this).index()).attr("width") === undefined) {
-        //             canvas_width[1] = $(this).find(".wrap").outerWidth();
-        //             canvas_height[1] = $(this).find(".wrap").outerHeight();
-        //             // while (canvas_height[1] === 53.976) {
-        //             //     canvas_height[1] = $(this).find(".wrap").outerHeight();
-        //             // }
-        //             $(this).find(".wrap #li" + $(this).index()).prop({
-        //                 "width": canvas_width[1] + 1,
-        //                 "height": canvas_height[1]
-        //             });
-        //             $(this).find(".wrap #li" + $(this).index()).css({
-        //                 "top": 15 + "px",
-        //                 "left": 15 + "px"
-        //             });
-        //             light($("#li" + $(this).index())[0]);
-        //         }
-        //         // })
-        //     })
-        // }, 200)
     }
 
     // 更新图片列的数量
     function update_img() {
-        // location.reload();
         minHeight = 0;
         indexHeight = 0;
         $waterfall = $("#page2 .waterfall");
@@ -134,11 +66,6 @@ $(function() {
         waterfall = $ul_img.width();
         cols_img = Math.floor(waterfall / img_width) || 1;
         colChange();
-        // $ul_img.css({
-        //     "width": `${ul_width}%`,
-        //     "margin": `0 ${(100-ul_width)/2}%`,
-        //     "transform": `translate(${-paddingofhalf}px)`,
-        // });
         $ul_img.css({
             "width": ul_width + "px",
         });
@@ -164,10 +91,9 @@ $(function() {
         if (minHeight < ($waterfall.scrollTop() + $waterfall.height())) {
             let timer = null;
             count = $ul_img.find("li").length;
-            imgName = Math.ceil(Math.random() * 84);
             $('<li>' +
                 '<div class="wrap">' +
-                '<img src="./assets/images/' + imgName + '.jpg" alt="快乐">' +
+                '<img src="./assets/images/' + Math.ceil(Math.random() * 84) + '.jpg" alt="快乐">' +
                 '<div class="message">' +
                 '<h4>图片' + count + '</h4>' +
                 '<p>中秋国庆快乐</p>' +
@@ -175,19 +101,19 @@ $(function() {
                 '<canvas id="li' + (count - 1) + '"></canvas>' +
                 '</div>' +
                 '</li>').insertBefore($li_last);
-            // refresh();
-            // 等immg加载出来在渲染位置
-            if ($li_last.find("li:not(.is-loding) img").height() !== 0) {
+            // 等img加载出来在渲染位置
+            if ($ul_img.find("li:not(.is-loding) img").height() !== 0) {
                 clearInterval(timer);
                 refresh();
             } else {
                 // 如果图片未加载成功开启定时器,知道img加载完成为止
                 timer = setInterval(() => {
-                    if ($li_last.find("li:not(.is-loding) img").height() !== 0) {
+                    console.log("图片加载中");
+                    if ($ul_img.find("li:not([is-loding]) img").height() !== 0) {
                         clearInterval(timer);
                     }
                     refresh();
-                }, 1000)
+                }, 100)
             }
         }
     }
@@ -237,7 +163,7 @@ $(function() {
             if (translateY < 3) {
                 translateY++;
                 scale += 0.01;
-                if (li[0].className !== "waterfall") {
+                if (li[0].tagName === "LI") {
                     li.css({
                         transform: "translateY(" + translateY + "px) scale(" + scale + ")",
                     });
@@ -269,88 +195,4 @@ $(function() {
             }
         }, 10)
     });
-
-
-    // let $item = $("#page1 .row .row_item");
-
-    // let canvas_width = [0, 0];
-    // let canvas_height = [0, 0];
-
-    // // section1
-    // $item.each(function() {
-    //     canvas_width[0] = $(this).find(".border").outerWidth();
-    //     canvas_height[0] = $(this).find(".border").outerHeight();
-    //     $(this).find(".border").append($("<canvas id=item" + $(this).index() + " width=" + canvas_width[0] + " height=" + canvas_height[0] + "></canvas>"));
-    //     $("#item" + $(this).index()).css({
-    //         "top": -1.5 + "px",
-    //         "left": -1.5 + "px"
-    //     });
-    //     light($("#item" + $(this).index())[0]);
-    // })
-
-    // $window.resize(function() {
-    //     $item.each(function() {
-    //         canvas_width[0] = $(this).find(".border").outerWidth()
-    //         canvas_height[0] = $(this).find(".border").outerHeight()
-    //         $(this).find(".border canvas").css({
-    //             "width": canvas_width[0] + 1 + "px",
-    //             "height": canvas_height[0] + "px"
-    //         })
-    //     })
-    // })
-
-    // function light(canvas) {
-    //     let ctx = canvas.getContext("2d");
-
-    //     let linear_width = 0.04; //发光边框的宽度
-    //     let linear_light = 0.9; //发光边框的亮度
-    //     let center_light = 0.1; //canvas中间的亮度
-
-    //     canvas.style.boxShadow = "0 0 10px rgba(28,163,203," + linear_light + ")";
-
-    //     // 左右渐变
-    //     let linear = ctx.createLinearGradient(0, 0, canvas.width, 0);
-    //     linear.addColorStop(0, "rgba(28,163,203," + linear_light + ")");
-    //     linear.addColorStop(linear_width, "rgba(2,38,83," + center_light + ")");
-    //     linear.addColorStop(1 - linear_width, "rgba(2,38,83," + center_light + ")");
-    //     linear.addColorStop(1, "rgba(28,163,203," + linear_light + ")");
-    //     ctx.fillStyle = linear;
-    //     ctx.beginPath();
-    //     ctx.moveTo(0, 0);
-    //     ctx.lineTo(canvas.width, canvas.height);
-    //     ctx.lineTo(canvas.width, 0);
-    //     ctx.lineTo(0, canvas.height)
-    //     ctx.moveTo(0, 0);
-    //     ctx.closePath();
-    //     ctx.fill();
-
-    //     // 上下渐变
-    //     linear = ctx.createLinearGradient(0, 0, 0, canvas.height);
-    //     linear.addColorStop(0, "rgba(28,163,203," + linear_light + ")");
-    //     linear.addColorStop(linear_width, "rgba(2,38,83," + center_light + ")");
-    //     linear.addColorStop(1 - linear_width, "rgba(2,38,83," + center_light + ")");
-    //     linear.addColorStop(1, "rgba(28,163,203," + linear_light + ")");
-    //     ctx.fillStyle = linear;
-    //     ctx.beginPath();
-    //     ctx.moveTo(0, 0);
-    //     ctx.lineTo(canvas.width, canvas.height);
-    //     ctx.lineTo(0, canvas.height)
-    //     ctx.lineTo(canvas.width, 0);
-    //     ctx.moveTo(0, 0);
-    //     ctx.closePath();
-    //     ctx.fill();
-    // }
-
-
-    // start(); // 一开始没有滚动的时候，出现在视窗中的图片也会加载
-
-    // function start() {
-    //     $("#page2 .waterfall li:not(:last-child) img").not('[data-isLoading]').each(function() {
-    //         if (minHeight - 200 < ($waterfall.scrollTop() + $waterfall.height())) {
-    //             $(this).attr('src', $(this).attr('data-src'));
-    //             // 已经加载的图片，给它设置一个属性，值为1，作为标识
-    //             $(this).attr('data-isLoading', 1);
-    //         }
-    //     })
-    // }
 })
